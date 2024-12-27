3588e1def4a896f509ca453a02702596491ead82
derikpimentel <derikpimentel@hotmail.com.br>
2024-12-27 14:05:27 -0300

    Second commit

--- WMI.py
+++ WMI.py
@@ -1,7 +1,84 @@
+from typing import List
 import wmi
 
+wmi_classes = [
+    {
+        'name': "Win32_Processor",
+        'properties': ["Name"]
+    },
+    {
+        'name': "Win32_PhysicalMemory",
+        'properties': ["Capacity", "Manufacturer", "PartNumber", "SerialNumber", "SMBIOSMemoryType", "Speed"]
+    },
+    {
+        'name': "Win32_CDROMDrive",
+        'properties': ["Name"]
+    },
+    {
+        'name': "Win32_DiskDrive",
+        'properties': ["MediaType", "Model", "SerialNumber", "Size"]
+    },
+    {
+        'name': "Win32_VideoController",
+        'properties': ["AdapterRAM", "Name", "VideoProcessor"]
+    },
+    {
+        'filter': "PhysicalAdapter = '1' AND PNPDeviceID LIKE 'PCI\\%'",
+        'name': "Win32_NetworkAdapter",
+        'properties': ["MACAddress", "Manufacturer", "Name", "NetConnectionID"]
+    },
+    {
+        'name': "Win32_BIOS",
+        'properties': ["Manufacturer", "Name", "SerialNumber", "Version"]
+    },
+    {
+        'name': "Win32_ComputerSystem",
+        'properties': ["Domain", "Manufacturer", "Model", "Name", "SystemFamily", "TotalPhysicalMemory", "UserName", "Workgroup"]
+    },
+    {
+        'name': "Win32_OperatingSystem",
+        'properties': ["BuildNumber", "Caption", "Manufacturer", "OSLanguage", "SerialNumber"]
+    },
+    {
+        'name': "SoftwareLicensingService",
+        'properties': ["OA3xOriginalProductKey"]
+    },
+    {
+        'name': "Win32_Product",
+        'properties': ["InstallDate", "Name", "Vendor", "Version"]
+    },
+    {
+        'filter': "LicenseStatus = '1' AND Name LIKE '%Office%'",
+        'name': "SoftwareLicensingProduct",
+        'properties': ["Name", "PartialProductKey"]
+    },
+    {
+        'name': "AntiVirusProduct",
+        'namespace': "root\SecurityCenter2",
+        'properties': ["displayName"]
+    }
+]
+
+# Função para interromper
+def pause() -> None:
+    input("[ENTER]")
+
+# Função que estrutura uma string do comando SELECT para SQL
+def select_query_str(table: str, columns: List[str] = ["*"], where_clause: str = None) -> str:
+    if table:
+        columns_str = ", ".join(columns)
+        where_clause_str = ""
+        if where_clause:
+            where_clause_str = f" WHERE {where_clause}"
+        return f"SELECT {columns_str} FROM {table}{where_clause_str}"
+
+# Função que inicializa o WMI e realiza uma busca otimizada
+def wmi_object(select_query: str, namespace: str = None) -> wmi:
+    if select_query:
+        return wmi.WMI(namespace=namespace).query(select_query)
+
 # Função para visualização
-def wmi_print(object_list):
+def wmi_print(object_list: wmi) -> None:
     for obj in object_list:
         properties = list(obj._properties) # Lista os atributos
 
@@ -19,33 +96,12 @@ def wmi_print(object_list):
         print(f"}};\n")
 
 if __name__ == "__main__":
-    c = wmi.WMI()
-    d = wmi.WMI(namespace="root\SecurityCenter2")
-
-    cpu = c.query("SELECT Name FROM Win32_Processor")
-    memory = c.query("SELECT Capacity, Manufacturer, PartNumber, SerialNumber, SMBIOSMemoryType, Speed FROM Win32_PhysicalMemory")
-    cd_rom = c.query("SELECT Name FROM Win32_CDROMDrive")
-    disk_drive = c.query("SELECT MediaType, Model, SerialNumber, Size FROM Win32_DiskDrive")
-    gpu = c.query("SELECT AdapterRAM, Name, VideoProcessor FROM Win32_VideoController")
-    net = c.query("SELECT MACAddress, Manufacturer, Name, NetConnectionID FROM Win32_NetworkAdapter WHERE PhysicalAdapter = '1' AND PNPDeviceID LIKE 'PCI\\%'")
-    bios = c.query("SELECT Manufacturer, Name, SerialNumber, Version FROM Win32_BIOS")
-    pc = c.query("SELECT Domain, Manufacturer, Model, Name, SystemFamily, TotalPhysicalMemory, UserName, Workgroup FROM Win32_ComputerSystem")
-    os = c.query("SELECT BuildNumber, Caption, Manufacturer, OSLanguage, SerialNumber FROM Win32_OperatingSystem")
-    key = c.query("SELECT OA3xOriginalProductKey FROM SoftwareLicensingService")
-    product = c.query("SELECT InstallDate, Name, Vendor, Version FROM Win32_Product")
-    partial_key = c.query("SELECT Name, PartialProductKey FROM SoftwareLicensingProduct WHERE LicenseStatus = '1' AND Name LIKE '%Office%'")
-    security = d.query("SELECT displayName FROM AntiVirusProduct")
-
-    wmi_print(cpu)
-    wmi_print(memory)
-    wmi_print(cd_rom)
-    wmi_print(disk_drive)
-    wmi_print(gpu)
-    wmi_print(net)
-    wmi_print(bios)
-    wmi_print(pc)
-    wmi_print(os)
-    wmi_print(key)
-    wmi_print(product)
-    wmi_print(partial_key)
-    wmi_print(security)
+    for wmi_class in wmi_classes:
+        table = wmi_class.get('name')
+        columns = wmi_class.get('properties')
+        where_clause = wmi_class.get('filter', None)
+        select_query = select_query_str(table, columns, where_clause)
+        namespace = wmi_class.get('namespace', None)
+        object_list = wmi_object(select_query, namespace)
+        wmi_print(object_list)
+        pause()
